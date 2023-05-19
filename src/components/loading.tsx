cffdf28aa91724d75c93c4326b1367554ecf54b5
zogss <yanlucasbranco@gmail.com>
2023-05-19 14:39:01 -0300

    feat: simple spinner added

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+export const SimpleSpinner = () => (
+  <svg
+    className="h-5 w-5 animate-spin"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      stroke-width="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
+  </svg>
+);
+
 export const LoadingSpinner = () => (
-  <span className="loader relative block h-[1em] w-[1em] overflow-hidden rounded-[50%] indent-[-9999em] text-[45px] text-[#b8b8b8]" />
+  <span className="loader relative block h-[1em] w-[1em] overflow-hidden rounded-[50%] indent-[-9999em] text-[45px] text-slate-300" />
 );
 
 export const LoadingPage = () => (
