c8d1e92f3d0796c125ce4488772a22922ecb65fd
zogss <yanlucasbranco@gmail.com>
2023-05-18 23:09:34 -0300

    feat: posts model

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,8 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const { isSignedIn } = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -19,6 +18,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!!isSignedIn ? <SignOutButton /> : <SignInButton />}</div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
