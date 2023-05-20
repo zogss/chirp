8721a27a1d1a54728a44a0bb1409a9c3326086c8
zogss <yanlucasbranco@gmail.com>
2023-05-20 18:25:19 -0300

    chore: layout changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,17 @@
-import { SignIn, useUser } from "@clerk/nextjs";
+import { SignInButton, SignedOut } from "@clerk/nextjs";
 import React, { type PropsWithChildren } from "react";
 import { MainHeader } from "./header";
 
-export const PageLayout = ({ children }: PropsWithChildren) => {
-  //* hooks
-  const { isSignedIn } = useUser();
-
-  //* render
-  return (
-    <main className="flex min-h-screen justify-center">
-      <MainHeader />
-      <div className="flex grow flex-col border-x border-slate-700 md:max-w-2xl">
-        {children}
-      </div>
-      <div className="flex grow">{!isSignedIn && <SignIn />}</div>
-    </main>
-  );
-};
+export const PageLayout = ({ children }: PropsWithChildren) => (
+  <main className="flex min-h-screen justify-center">
+    <MainHeader />
+    <div className="flex grow flex-col border-x border-slate-700 md:max-w-2xl">
+      {children}
+    </div>
+    <div className="flex grow">
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+    </div>
+  </main>
+);
