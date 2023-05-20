38786c57f95206bb463d18a82b240a9d336b6af3
zogss <yanlucasbranco@gmail.com>
2023-05-19 23:29:55 -0300

    styles: custom shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "outline-white":
+          "rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
