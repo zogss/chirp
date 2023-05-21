63408468e36d294593be93f117a2c6f8ab23b086
zogss <yanlucasbranco@gmail.com>
2023-05-21 13:39:08 -0300

    feat: adding custom styles in clerk components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,30 @@ import { api } from "~/utils/api";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
 
+const elements = {
+  card: "bg-black shadow-outline-white text-white",
+  headerTitle: "text-white",
+  headerSubtitle: "text-gray-400",
+  modalCloseButton: "text-white",
+  socialButtonsBlockButton:
+    "bg-black text-white border border-slate-400 hover:bg-white hover:bg-opacity-10",
+  dividerLine: "bg-slate-400",
+  dividerText: "text-slate-400",
+  formFieldLabel: "text-white",
+  formFieldInput: "bg-black text-white border-slate-400 placeholder-gray-400",
+  formButtonPrimary:
+    "bg-black shadow-outline-white text-white hover:bg-white hover:bg-opacity-10",
+  footerActionText: "text-white",
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      {...pageProps}
+      appearance={{
+        elements,
+      }}
+    >
       <Head>
         <title>Chirp</title>
         <meta name="description" content="💭" />
