67eced844cbdc1ce6bdc7c380ab39c5d96a8770c
zogss <yanlucasbranco@gmail.com>
2023-05-21 00:15:48 -0300

    feat: using custom hook for better responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,12 +16,12 @@ const HeaderItems = () => {
   if (!isSignedIn) return null;
 
   return (
-    <div className="flex flex-col items-start justify-start gap-2">
+    <div className="flex flex-col items-start justify-end gap-2 md:justify-start">
       <Link
         href="/"
         aria-label="Go to home page"
         className={clsx(
-          "flex items-center gap-4 rounded-full py-2.5 pl-4 pr-8 text-base transition-all hover:bg-slate-200 hover:bg-opacity-10",
+          "flex items-center gap-4 rounded-full p-2.5 text-base transition-all hover:bg-slate-200 hover:bg-opacity-10 md:py-2.5 md:pl-4 md:pr-8",
           {
             "font-bold text-white": router.asPath === "/",
             "font-semibold text-gray-300": router.asPath !== "/",
@@ -33,13 +33,13 @@ const HeaderItems = () => {
         ) : (
           <AiOutlineHome size={32} />
         )}
-        Home
+        <span className="hidden md:block">Home</span>
       </Link>
       <Link
         href={`/${(isLoaded && user.username) || ""}`}
         aria-label="Go to profile page"
         className={clsx(
-          "flex items-center gap-4 rounded-full py-2.5 pl-4 pr-8 text-base transition-all hover:bg-slate-200 hover:bg-opacity-10",
+          "flex items-center gap-4 rounded-full p-2.5 text-base transition-all hover:bg-slate-200 hover:bg-opacity-10 md:py-2.5 md:pl-4 md:pr-8",
           {
             "font-bold text-white":
               isLoaded &&
@@ -57,19 +57,15 @@ const HeaderItems = () => {
         ) : (
           <BsFillPersonFill size={32} />
         )}
-        Profile
+        <span className="hidden md:block">Profile</span>
       </Link>
     </div>
   );
 };
 
-export const MainHeader = () => {
-  return (
-    <div className="flex h-screen grow flex-col items-end">
-      <div className="flex w-fit flex-grow flex-col items-start justify-between pb-4 pr-4 pt-8">
-        <HeaderItems />
-        <AccountMenu />
-      </div>
-    </div>
-  );
-};
+export const MainHeader = () => (
+  <div className="flex w-fit flex-grow flex-col items-start justify-between pb-4 pl-4 pr-4 pt-8 md:pl-0">
+    <HeaderItems />
+    <AccountMenu />
+  </div>
+);
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,17 +1,41 @@
-import { SignInButton, SignedOut } from "@clerk/nextjs";
 import React, { type PropsWithChildren } from "react";
+import { useLayout } from "~/hooks/useLayout";
 import { MainHeader } from "./header";
+import { SideBar } from "./sideBar";
 
-export const PageLayout = ({ children }: PropsWithChildren) => (
-  <main className="flex min-h-screen justify-center">
-    <MainHeader />
-    <div className="flex grow flex-col border-x border-slate-700 md:max-w-2xl">
-      {children}
-    </div>
-    <div className="flex grow">
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-    </div>
-  </main>
-);
+export const PageLayout = ({ children }: PropsWithChildren) => {
+  //* hooks
+  const {
+    columnsWidth: { leftWidth, mainWidth, rightWidth },
+  } = useLayout();
+
+  //* render
+  return (
+    <main className="flex min-h-screen justify-center">
+      <div
+        className="flex min-h-screen grow flex-col items-end"
+        style={{
+          width: leftWidth,
+        }}
+      >
+        <MainHeader />
+      </div>
+      <div
+        className="flex grow flex-col border-x border-slate-700 md:max-w-2xl"
+        style={{
+          width: mainWidth,
+        }}
+      >
+        {children}
+      </div>
+      <div
+        className="hidden min-h-screen grow flex-col items-start sm:flex"
+        style={{
+          width: rightWidth,
+        }}
+      >
+        <SideBar />
+      </div>
+    </main>
+  );
+};
