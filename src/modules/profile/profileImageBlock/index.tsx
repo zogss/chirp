3052461ed4ee9ce5ab8252002c1ff96fce5a70cc
zogss <yanlucasbranco@gmail.com>
2023-05-25 14:13:25 -0300

    fix: profile image size fixed

--- src/modules/profile/profileImageBlock/index.tsx
+++ src/modules/profile/profileImageBlock/index.tsx
@@ -5,19 +5,17 @@ import Image from "next/image";
 export const ProfileImageBlock = ({
   profileImageUrl,
   username,
-}: Partial<User>) => {
-  return (
-    <div>
-      <div className="relative h-40 bg-slate-600">
-        <Image
-          src={profileImageUrl || ""}
-          alt={`${username || ""} - profile image`}
-          width={128}
-          height={128}
-          className="absolute bottom-0 left-0 -mb-16 ml-4 rounded-full border-4 border-black"
-        />
-      </div>
-      <div className="h-16" />
+}: Partial<User>) => (
+  <div>
+    <div className="relative h-40 bg-slate-600">
+      <Image
+        src={profileImageUrl || ""}
+        alt={`${username || ""} - profile image`}
+        width={128}
+        height={128}
+        className="absolute bottom-0 left-0 -mb-16 ml-4 h-32 w-32 rounded-full border-4 border-black"
+      />
     </div>
-  );
-};
+    <div className="h-16" />
+  </div>
+);
